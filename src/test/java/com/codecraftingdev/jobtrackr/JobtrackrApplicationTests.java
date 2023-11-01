83800e322815b422fc3523c66d17d8a9548c373f
Code Crafting Dev <codecraftingdev@gmail.com>
2023-11-01 16:48:09 +0100

    Initial commit

---
+++ src/test/java/com/codecraftingdev/jobtrackr/JobtrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecraftingdev.jobtrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
